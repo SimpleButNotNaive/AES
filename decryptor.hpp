4283c0a758d2c149facdf0ba5f6dc68ed96ba442
huzr <huzhui.x@gmail.com>
2019-10-21 22:25:06 +0800

    最终版本

--- decryptor.hpp
+++ decryptor.hpp
@@ -4,19 +4,19 @@
 #include "aes_abstract.hpp"
 
 class aes_decryptor : public aes_abstract{
-public:
 
     void SubBytes();
     void MixColumns();
-
     uint8 SubByte(uint8);
 
     void ShiftRows();
     void reverse_round_key();
-    string decryption(vector<vector<uint8>> cipher, string key, string init_vector);
     void splice(uint8*, uint8[16]);
     int truncate(uint8* p_text_array, int size);
     void decrypt_one_group(uint8 cipher[16]);
+
+public:
+    string decryption(vector<vector<uint8>> cipher, string key, string init_vector);
 };
 
 #endif
